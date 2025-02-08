bbb9accdfd2c8f916e721322e5925369d3a8b52c
sinister-kid <100430877+sinister-kid@users.noreply.github.com>
2025-02-08 04:16:01 +0100

    Little changes

--- OnePaceES/src/main/kotlin/com/Phisher98/OnePacePlugin.kt
+++ OnePaceES/src/main/kotlin/com/Phisher98/OnePacePlugin.kt
@@ -4,7 +4,7 @@ import com.lagradost.cloudstream3.plugins.BasePlugin
 import com.lagradost.cloudstream3.plugins.CloudstreamPlugin
 
 @CloudstreamPlugin
-class OnePacePlugin : BasePlugin() {
+class OnePaceESPlugin : BasePlugin() {
     override fun load() {
         registerMainAPI(OnePaceES())
     }
